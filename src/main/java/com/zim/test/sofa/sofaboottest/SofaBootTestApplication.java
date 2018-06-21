1f988eb46b7ab045d4545b2d43d047a7590280e9
zimliu <cngddflzw@gmail.com>
2018-06-21 10:55:52 +0800

    init

---
+++ src/main/java/com/zim/test/sofa/sofaboottest/SofaBootTestApplication.java
@@ -0,0 +1,12 @@
+package com.zim.test.sofa.sofaboottest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SofaBootTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SofaBootTestApplication.class, args);
+	}
+}
