290c1a84869f782118601f435f7dbd20a9bbb4cb
zimliu <cngddflzw@gmail.com>
2018-06-28 16:58:12 +0800

    test rpc for ark

--- src/main/java/com/zim/test/sofa/sofaboottest/SofaBootTestApplication.java
+++ src/main/java/com/zim/test/sofa/sofaboottest/SofaBootTestApplication.java
@@ -2,8 +2,10 @@ package com.zim.test.sofa.sofaboottest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource({ "classpath*:rpc-starter-example.xml" })
 public class SofaBootTestApplication {
 
 	public static void main(String[] args) {
--- src/test/java/com/zim/test/sofa/sofaboottest/SofaBootTestApplicationTests.java
+++ src/test/java/com/zim/test/sofa/sofaboottest/SofaBootTestApplicationTests.java
@@ -1,16 +1,28 @@
 package com.zim.test.sofa.sofaboottest;
 
+import com.alipay.sofa.ark.springboot.runner.ArkBootRunner;
+import com.zim.test.sofa.sofaboottest.bean.PersonService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@RunWith(ArkBootRunner.class)
+//@RunWith(SpringRunner.class)
+@SpringBootTest(classes = SofaBootTestApplication.class)
 public class SofaBootTestApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Autowired
+    private PersonService personReferenceBolt;
+
+    @Autowired
+    private PersonService personReferenceRest;
+
+    @Test
+    public void contextLoads() {
+        System.out.println(personReferenceBolt.sayName("abc"));
+        System.out.println(personReferenceRest.sayName("123"));
+    }
 
 }
